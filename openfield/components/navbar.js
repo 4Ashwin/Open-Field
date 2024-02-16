d2f9fba31270d6f694a83361a1808c6c36478047
ASHWIN BINU ABRAHAM <ashwinbinu2002@gmail.com>
2024-02-17 02:19:09 +0530

    map iframe

--- openfield/components/navbar.js
+++ openfield/components/navbar.js
@@ -5,7 +5,7 @@ function Navbar(props) {
     return (
 
 
-        <div className="navbar bg-base-100 max-w-screen w-[98%]">
+        <div className="navbar bg-base-100 max-w-screen  fixed top-0 left-0 w-full">
             <div className="flex-1">
                 <Link href={"/openfield"} className="btn btn-ghost text-xl">OpenField</Link>
             </div>
--- openfield/pages/openfield/index.js
+++ openfield/pages/openfield/index.js
@@ -5,8 +5,8 @@ function openfield(props) {
     return (
         <div>
             <Navbar />
-            <div>
-
+            <div className='w-screen h-screen'>
+                <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Bengaluru+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <a href='https://www.krankenversicherungsvergleich.at/'>Zusatzversicherung Vergleich</a> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=9ab34e5f13f68669c6803a130144e0e2aa069edb'></script>
             </div>
         </div>
     );
