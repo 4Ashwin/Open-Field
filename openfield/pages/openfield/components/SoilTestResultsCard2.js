5cd8fa4340cc0008ad621ae248cb17ec641d42bf
ASHWIN <75925525+4Ashwin@users.noreply.github.com>
2024-02-17 17:24:05 +0530

    ui changes

--- openfield/pages/openfield/components/SoilTestResultsCard2.js
+++ openfield/pages/openfield/components/SoilTestResultsCard2.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Pie } from 'react-chartjs-2';
 import { Chart } from 'chart.js/auto';
 
-const SoilTestResultsCard = () => {
+const SoilTestResultsCard2 = () => {
   const data = {
-    labels: ['Fertiliser A', 'Fertiliser B', 'FertiliserD', 'Fertiliser C'],
+    labels: ['producer A', 'producer B', 'producer C'],
     datasets: [
       {
-        data: [35, 20, 10, 35],
-        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#FFDE56'],
-        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#FFDE56'],
+        data: [45, 20, 35],
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
       },
     ],
   };
@@ -18,7 +18,7 @@ const SoilTestResultsCard = () => {
     <div className="block max-w-[18rem] border-2 rounded-lg bg-white text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
       <div className="p-6">
         <h5 className="mb-1 text-xl font-medium leading-tight text-black">
-          Fertilisers Usage
+          Overall Producer List
         </h5>
         <div className="mb-4">
           <Pie data={data} />
@@ -28,4 +28,4 @@ const SoilTestResultsCard = () => {
   );
 };
 
-export default SoilTestResultsCard;
+export default SoilTestResultsCard2;
