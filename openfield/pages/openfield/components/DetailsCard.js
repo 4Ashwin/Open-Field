29db259a8c8cef0b8927b6ec3253996af0c1e8b5
ASHWIN <75925525+4Ashwin@users.noreply.github.com>
2024-02-17 10:18:19 +0530

    famer-profile

--- openfield/pages/openfield/components/DetailsCard.js
+++ openfield/pages/openfield/components/DetailsCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const DetailsCard = ({ userType, farmerName, farmLocation, cropInfo }) => (
-  <div className="block max-w-[18rem] rounded-lg bg-white text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
+  <div className="block max-w-[18rem] ml-10 border-2 rounded-lg bg-white text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
     <div className="p-6">
       <h5 className="mb-1 text-xl font-medium leading-tight text-black">
         {userType} Details
